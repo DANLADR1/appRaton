18cf54a918fdb7e2f77ba7828b0924dd3f15b342
Adrian <adrianmorenomartos@gmail.com>
2024-06-17 14:28:35 +0200

    1.2

--- src/mijava/paquete/ratonera.java
+++ src/mijava/paquete/ratonera.java
@@ -1,11 +1,12 @@
 package mijava.paquete;
 
-public class ratonera extends ratonera2 {
+public class ratonera extends ratonera2 implements ratonera3 {
 
     public ratonera(){
     }
 
     // Este método devuelve formateada la edad y peso del ratón.
+    @Override
     public String infromacion() {
         return String.format("(%d;%.2f)", this.getEdad(), this.getPeso());
     }
---
+++ src/mijava/paquete/ratonera3.java
@@ -0,0 +1,6 @@
+package mijava.paquete;
+
+public interface ratonera3 {
+    // Este método devuelve formateada la edad y peso del ratón.
+    String infromacion();
+}
